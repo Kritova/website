18f4ac3be9286aca774824b71803332e45f91d05
gogineni1708 <gogineni1708@gmail.com>
2025-04-15 21:52:38 -0500

    🚀 Updated homepage and fixed routing issues

--- client/src/pages/Investors.jsx
+++ client/src/pages/Investors.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Investors from './pages/Investors';
+import React from 'react';
+
 
 function App() {
   return (
